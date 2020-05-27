e3e5c27ea04c531457e154673d5148bb1e6ca4d6
Dinu <dinurp@yahoo.com>
2020-05-27 10:06:42 +0400

    Added Logging

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os
-import json
 
 from flask import Flask, request, make_response, send_file, Response
 from flask import json
@@ -7,6 +6,8 @@ from time  import time
 
 import pytesseract
 
+import logging 
+logger = logging.getLogger('tesseract-in-cf')
 app = Flask(__name__)
 
 #health check
@@ -23,6 +24,7 @@ def ocr_test():
     file = request.args.get('image','sample/fox.png')
     lang = request.args.get('lang', 'eng')
     config = request.args.get('config', '--psm 11')
+    logger.info("pytesseract.image_to_string({},lang={},config={})".format(file,lang,config))
     text = pytesseract.image_to_string(file,lang=lang,config=config)
     response = make_response(text)
     response.headers['Content-Type'] = 'text/plain'
@@ -43,10 +45,11 @@ def ocr_files(folder=None):
     uploaded_files = request.files.getlist('image')
     saved_files = []
     for file in uploaded_files:  
-        print("Posted file: {}".format(file))
+        logger.info("Posted file: {}".format(file))
         if file.filename.split('.')[-1] in ['jpeg','png','jpg','pdf','gif','jfif']:
             saved_file = getSavedFilepath(folder,file.filename) 
             file.save( saved_file )
+            logger.info("pytesseract.image_to_string({},lang={},config={})".format(saved_file,lang,config))
             text = pytesseract.image_to_string(saved_file,lang=lang,config=config)
             saved_files.append({"file":file.filename,"saved_as":saved_file, "text":text}) 
     response = make_response(json.dumps(saved_files))
@@ -55,6 +58,13 @@ def ocr_files(folder=None):
 
 
 if __name__ == '__main__':
+    import logging.config
+    import yaml
+
+    with open('logger-config.yaml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+
     port = int(os.environ.get('PORT', 3000))
     host = os.environ.get('VCAP_APP_HOST', '0.0.0.0')
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
